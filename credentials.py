8627fa7987ce426b4732d300b7eed17c7830eea4
Anto Joel <51856652+Antojoel@users.noreply.github.com>
2021-06-02 12:00:39 +0530

    just add the below line in the top
    import credentials as cds

---
+++ credentials.py
@@ -0,0 +1,9 @@
+user_id = 'enter mail here'
+user_password = 'enter password'
+
+
+#more additional functionalities if needed...
+#if needed assigning a name if you have multiple mail id to send messages
+name1 = 'mailid 1'
+name2 = 'mailid 2'
+#list goes on.......
